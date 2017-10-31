be25bfc056b276b70d736fc76192e0018f7b2ca1
Aleksandr <ovcharenkooleksandr@gmail.com>
2017-10-31 19:48:54 +0300

    TestProject

---
+++ TestTask/Models/PersonInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace TestTask.Models
+{
+    public class PersonInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<ApplicationDbContext>
+    {
+        protected override void Seed(ApplicationDbContext context)
+        {
+            var personsList = new List<Person>
+            {
+                new Person{ Name="Aleksandr", Surname="Ovcharenko"},
+                new Person{ Name="Ivan", Surname="Ivanov"}
+            };
+            personsList.ForEach(s => context.Persons.Add(s));
+            context.SaveChanges();
+        }
+
+    }
+}
